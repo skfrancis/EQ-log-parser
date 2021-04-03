18673c03326a47ec6424367d7df3466e793d60a8
Shawn Francis <shawnkfrancis@gmail.com>
2021-04-03 01:28:22 -0500

    Incoming actions completed

--- trackers/fights/fightmember.py
+++ trackers/fights/fightmember.py
@@ -10,10 +10,11 @@ class FightMember:
             'total_outbound_dmg': 0,
             'total_hits': 0,
             'total_misses': 0,
-            'total_inbound_dmg': 0,
             'total_heals': 0,
             'total_healing': 0,
             'total_over_healing': 0,
+            'total_inbound_dmg': 0,
+            'total_inbound_misses': 0,
             'total_healed': 0,
             'total_deaths': 0
         }
@@ -21,24 +22,24 @@ class FightMember:
         self.outbound_misses = []
         self.outbound_heals = []
         self.inbound_dmg = []
+        self.inbound_misses = []
         self.inbound_heals = []
         self.duration = {
-            'first_attack': None,
-            'final_attack': None
+            'first_timestamp': None,
+            'final_timestamp': None
         }
         self.pet = None
+        self.debug = []
 
     def get_member_name(self):
         return self.name
 
     def add_hit(self, hit_data):
+        self.debug.append(hit_data)
         self._update_participation_time(hit_data.get('Timestamp'))
         self.totals['total_hits'] += 1
         self.totals['total_outbound_dmg'] += int(hit_data.get('Amount'))
-        if hit_data.get('Ability')[0].isupper():
-            ability = hit_data.get('Ability')
-        else:
-            ability = hit_data.get('Ability').capitalize()
+        ability = hit_data.get('Ability')
         mod = hit_data.get('Mod')
         if any(hit['Ability'] for hit in self.outbound_dmg if ability == hit['Ability']):
             index = [i for i, hit in enumerate(self.outbound_dmg) if ability == hit['Ability']]
@@ -48,7 +49,6 @@ class FightMember:
             hit['Damage'] += int(hit_data.get('Amount'))
             hit['Count'] += 1
             if mod:
-                mod = mod.capitalize()
                 hit[mod] = hit.setdefault(mod, 0) + 1
         else:
             data = {
@@ -58,17 +58,14 @@ class FightMember:
                 'Count': 1
             }
             if mod:
-                mod = mod.capitalize()
                 data[mod] = 1
             self.outbound_dmg.append(data)
 
     def add_damage(self, dmg_data):
+        self.debug.append(dmg_data)
         self._update_participation_time(dmg_data.get('Timestamp'))
         self.totals['total_inbound_dmg'] += int(dmg_data.get('Amount'))
-        if dmg_data.get('Ability')[0].isupper():
-            ability = dmg_data.get('Ability')
-        else:
-            ability = dmg_data.get('Ability').capitalize()
+        ability = dmg_data.get('Ability')
         mod = dmg_data.get('Mod')
         if any(dmg['Ability'] for dmg in self.inbound_dmg if ability == dmg['Ability']):
             index = [i for i, dmg in enumerate(self.inbound_dmg) if ability == dmg['Ability']]
@@ -78,7 +75,6 @@ class FightMember:
             dmg['Damage'] += int(dmg_data.get('Amount'))
             dmg['Count'] += 1
             if mod:
-                mod = mod.capitalize()
                 dmg[mod] = dmg.setdefault(mod, 0) + 1
         else:
             data = {
@@ -88,46 +84,60 @@ class FightMember:
                 'Count': 1
             }
             if mod:
-                mod = mod.capitalize()
                 data[mod] = 1
             self.inbound_dmg.append(data)
 
     def add_miss(self, miss_data):
+        self.debug.append(miss_data)
         self._update_participation_time(miss_data.get('Timestamp'))
         self.totals['total_misses'] += 1
-        if miss_data.get('Ability')[0].isupper():
-            ability = miss_data.get('Ability')
-        else:
-            ability = miss_data.get('Ability').capitalize()
+        ability = miss_data.get('Ability')
         mod = miss_data.get('Mod')
         if any(miss['Ability'] for miss in self.outbound_misses if ability == miss['Ability']):
             index = [i for i, miss in enumerate(self.outbound_misses) if ability == miss['Ability']]
             miss = self.outbound_misses[index[0]]
             if mod:
-                mod = mod.capitalize()
                 miss[mod] = miss.setdefault(mod, 0) + 1
             else:
-                mod = miss_data.get('Amount').capitalize()
+                mod = miss_data.get('Amount')
                 miss[mod] = miss.setdefault(mod, 0) + 1
         else:
-            if mod:
-                mod = mod.capitalize()
-            else:
-                mod = miss_data.get('Amount').capitalize()
+            if not mod:
+                mod = miss_data.get('Amount')
             self.outbound_misses.append({
                 'Ability': ability,
                 mod: 1
             })
 
+    def add_avoidance(self, avoid_data):
+        self.debug.append(avoid_data)
+        self._update_participation_time(avoid_data.get('Timestamp'))
+        self.totals['total_inbound_misses'] += 1
+        ability = avoid_data.get('Ability')
+        mod = avoid_data.get('Mod')
+        if any(miss['Ability'] for miss in self.inbound_misses if ability == miss['Ability']):
+            index = [i for i, miss in enumerate(self.inbound_misses) if ability == miss['Ability']]
+            miss = self.inbound_misses[index[0]]
+            if mod:
+                miss[mod] = miss.setdefault(mod, 0) + 1
+            else:
+                mod = avoid_data.get('Amount')
+                miss[mod] = miss.setdefault(mod, 0) + 1
+        else:
+            if not mod:
+                mod = avoid_data.get('Amount')
+            self.inbound_misses.append({
+                'Ability': ability,
+                mod: 1
+            })
+
     def add_heal(self, heal_data):
+        self.debug.append(heal_data)
         self._update_participation_time(heal_data.get('Timestamp'))
         self.totals['total_heals'] += 1
         self.totals['total_healing'] += int(heal_data.get('Amount')[0])
         self.totals['total_over_healing'] += (int(heal_data.get('Amount')[1]) - int(heal_data.get('Amount')[0]))
-        if heal_data.get('Ability')[0].isupper():
-            ability = heal_data.get('Ability')
-        else:
-            ability = heal_data.get('Ability').capitalize()
+        ability = heal_data.get('Ability')
         mod = heal_data.get('mod')
         if any(heal['Ability'] for heal in self.outbound_heals if ability == heal['Ability']):
             index = [i for i, heal in enumerate(self.outbound_heals) if ability == heal['Ability']]
@@ -140,7 +150,6 @@ class FightMember:
             heal['Overage'] += (int(max_heal) - int(actual_heal))
             heal['Count'] += 1
             if mod:
-                mod = mod.capitalize()
                 heal[mod] = heal.setdefault(mod, 0) + 1
         else:
             actual_heal = heal_data.get('Amount')[0]
@@ -153,17 +162,14 @@ class FightMember:
                 'Count': 1
             }
             if mod:
-                mod = mod.capitalize()
                 data[mod] = 1
             self.outbound_heals.append(data)
 
     def add_healing(self, healed_data):
+        self.debug.append(healed_data)
         self._update_participation_time(healed_data.get('Timestamp'))
         self.totals['total_healed'] += int(healed_data.get('Amount')[0])
-        if healed_data.get('Ability')[0].isupper():
-            ability = healed_data.get('Ability')
-        else:
-            ability = healed_data.get('Ability').capitalize()
+        ability = healed_data.get('Ability')
         mod = healed_data.get('mod')
         if any(heal['Ability'] for heal in self.inbound_heals if ability == heal['Ability']):
             index = [i for i, heal in enumerate(self.inbound_heals) if ability == heal['Ability']]
@@ -172,7 +178,6 @@ class FightMember:
             heal['Actual'] += int(actual_heal)
             heal['Count'] += 1
             if mod:
-                mod = mod.capitalize()
                 heal[mod] = heal.setdefault(mod, 0) + 1
         else:
             actual_heal = healed_data.get('Amount')[0]
@@ -183,19 +188,20 @@ class FightMember:
                 'Count': 1
             }
             if mod:
-                mod = mod.capitalize()
                 data[mod] = 1
             self.inbound_heals.append(data)
 
     def add_death(self, death_data):
+        self.debug.append(death_data)
         self._update_participation_time(death_data.get('Timestamp'))
         self.totals['total_deaths'] += 1
 
     def get_fight_data(self):
-        index = ['Name', 'Class', 'Level', 'Duration',
-                 'Total Damage (OB)', 'Total Hits (OB)', 'Total Misses (OB)',
-                 'Total Heals (OB)', 'Total Actual Healing (OB)', 'Total Healing Overage (OB)',
-                 'Total Damage (IB)', 'Total Healing (IB)', 'Death Count'
+        index = [
+            'Name', 'Class', 'Level', 'Duration',
+            'Total Damage (OB)', 'Total Hits (OB)', 'Total Misses (OB)',
+            'Total Heals (OB)', 'Total Actual Healing (OB)', 'Total Healing Overage (OB)',
+            'Total Damage (IB)', 'Total Misses (IB)', 'Total Healing (IB)', 'Death Count'
         ]
         data = [
             self.name,
@@ -209,6 +215,7 @@ class FightMember:
             self.totals.get('total_healing'),
             self.totals.get('total_over_healing'),
             self.totals.get('total_inbound_dmg'),
+            self.totals.get('total_inbound_misses'),
             self.totals.get('total_healed'),
             self.totals.get('total_deaths'),
         ]
@@ -218,18 +225,22 @@ class FightMember:
             'misses': pd.DataFrame(self.outbound_misses).fillna(0, downcast='infer'),
             'heals': pd.DataFrame(self.outbound_heals).fillna(0, downcast='infer'),
             'damage': pd.DataFrame(self.inbound_dmg).fillna(0, downcast='infer'),
+            'avoidance': pd.DataFrame(self.inbound_misses).fillna(0, downcast='infer'),
             'healing': pd.DataFrame(self.inbound_heals).fillna(0, downcast='infer')
         }
 
     def _update_participation_time(self, timestamp):
-        if not self.duration.get('first_attack'):
-            self.duration['first_attack'] = timestamp
-        self.duration['final_attack'] = timestamp
+        if not self.duration.get('first_timestamp'):
+            self.duration['first_timestamp'] = timestamp
+        self.duration['final_timestamp'] = timestamp
 
     def _get_duration(self):
         duration = 1
-        if self.duration.get('final_attack') and self.duration.get('first_attack'):
-            actual_duration = self.duration.get('final_attack') - self.duration.get('first_attack')
+        if self.duration.get('first_timestamp') and self.duration.get('final_timestamp'):
+            actual_duration = self.duration.get('final_timestamp') - self.duration.get('first_timestamp')
             actual_duration = actual_duration.total_seconds()
             duration = actual_duration if int(actual_duration) > duration else duration
         return duration
+
+
+
